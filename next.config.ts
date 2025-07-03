d6484054708b0846333ba27b6fe54a3f5c262c0e
dharmik.jasoliya@tatvasoft.com <dharmik.jasoliya@tatvasoft.com>
2025-07-03 12:09:18 +0530

    fixed build failing issue

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -10,11 +9,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
