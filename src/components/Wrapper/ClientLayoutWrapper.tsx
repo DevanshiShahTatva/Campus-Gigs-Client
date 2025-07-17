e8ec6b937e6ca5653b03b8616559ba18d7df1181
RavirajSolanki <raviraj.solanki@tatvasoft.com>
2025-07-17 13:06:09 +0530

    commented code removed

--- src/components/Wrapper/ClientLayoutWrapper.tsx
+++ src/components/Wrapper/ClientLayoutWrapper.tsx
@@ -7,26 +7,6 @@ import { useEffect } from 'react';
 export function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  //     navigator.serviceWorker.register('/sw.js').then((registration) => {
-  //       // Send a static push notification every 10 seconds
-  //       const interval = setInterval(() => {
-  //         if (registration.active) {
-  //           console.log('Triggering static push notification');
-  //           registration.active.postMessage({
-  //             type: 'STATIC_PUSH',
-  //             title: 'Static Push Notification',
-  //             body: 'This is a demo notification shown every 10 seconds.',
-  //             url: '/'
-  //           });
-  //         }
-  //       }, 10000);
-  //       return () => clearInterval(interval);
-  //     });
-  //   }
-  // }, []);
-
   useEffect(() => {
     if (typeof window !== 'undefined' && 'Notification' in window) {
       if (Notification.permission === 'default') {
