6fc8c131006e37f6b2c3b2bd475a4c06404f4dc7
priteshtatvasoft <pritesh.makasana@tatvasoft.com>
2025-07-24 13:35:28 +0530

    Handle kyc button

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -797,14 +797,17 @@ const Profile = () => {
                         Edit
                       </button>
                     </div>
-                    {userProfile.profile_type == "provider" && (
+                    {
                       <div className="py-3">
                         <div className="font-medium text-gray-900 mb-1">
                           Stripe KYC
                         </div>
-                        <OnboardStripeButton providerId={userProfile.id} />
+                        {userProfile.profile_type == "provider" &&
+                          !userProfile.stripe_account_id ? (
+                            <OnboardStripeButton providerId={userProfile.id} />
+                          ) : <p className="font-600 text-[var(--base)]">KYC Done</p>}
                       </div>
-                    )}
+                    }
                   </div>
                 </div>
               )}
